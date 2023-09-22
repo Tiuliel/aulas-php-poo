7f4c41a9858dfe093c133ed7f4741ef6127ee413
Eliel Moura <eliellpf@gmail.com>
2023-09-22 09:13:24 -0300

    metodo

--- src/Cliente.php
+++ src/Cliente.php
@@ -1,18 +1,21 @@
 <?php
 class Cliente{
 
-    //Propriedades (ou atributos)
+    
     public string $nome;
-    public string $email = ""; // valor padrão
+    public string $email;
     public string $senha;
     public array $telefones;
+    
+    /* Método construtor permite configurar a forma de inicialização
+    do objeto, exigindo a atribuição de dados no momento de criar o 
+    objeto/instância. O construtor é sempre chamado de forma automático assim que o objeto é criado. */
 
-    //Métodos (ou comportamentos)
-    public function exibirDados():void {
-        echo "<section>";
-        echo "<h2> $this->nome</h2>";
-        echo "<p> $this->email</p>";
-        echo "</section>";
-
+    public function __construct(string $nome, string $email){
+     /* Atribuindo os valores dos parâmetros às propriedades. 
+     Usamos o $this para acessar as propriedades desta Classe. */
+        $this->nome = $nome;
+        $this->email = $email;
     }
+    
 }
